7c4c75291e46d63a4d47c44d20474196063ff255
madhavbanjade <madhavbnzd57@gmail.com>
2026-09-12 12:17:16 +0545

    changes

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,8 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  //upload files
+  //upload files  uwguwu33 
+  
   //changing while deplying
   app.useStaticAssets(join(process.cwd(), 'uploads'), {
     prefix: '/uploads',
